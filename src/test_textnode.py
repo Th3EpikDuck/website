ce5d53309e45cd490e52bf6278fcb816b4db97fe
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 16:17:54 -0700

    Create test_textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_diff_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("Different text", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_diff_type(self):
+        node = TextNode("Same text", TextType.BOLD)
+        node2 = TextNode("Same text", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_with_url(self):
+        node = TextNode("link", TextType.LINK, "https://boot.dev")
+        node2 = TextNode("link", TextType.LINK, "https://boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_diff_url(self):
+        node = TextNode("link", TextType.LINK, "https://a.com")
+        node2 = TextNode("link", TextType.LINK, "https://b.com")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
