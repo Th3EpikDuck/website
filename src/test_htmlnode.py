08ca548d91f518ea85dc68d71a894df5f9ac37bd
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-06 07:31:42 -0700

    Update test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -57,9 +57,9 @@ class TestLeafNode(unittest.TestCase):
 
 class TestParentNode(unittest.TestCase):
     def test_to_html_with_children(self):
-    child_node = LeafNode("span", "child")
-    parent_node = ParentNode("div", [child_node])
-    self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
 
     def test_to_html_with_grandchildren(self):
         grandchild_node = LeafNode("b", "grandchild")
