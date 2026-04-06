33ef0a763d112847c3812df06d285e250595aaf3
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-06 00:23:13 -0700

    Update htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(self, tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        if self.tag is None:
+            return str(self.value)
+        props_html = self.props_to_html()
+        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
