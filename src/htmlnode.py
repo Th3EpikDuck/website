72fddab3db9a99c1f49eeb06a4d294fbf97cfa76
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-06 07:25:44 -0700

    Update htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -41,5 +41,9 @@ class ParentNode(HTMLNode):
     def to_html(self):
         if self.tag is None:
             raise ValueError()
-        elif self.children is None:
+        if self.children is None or len(self.children) == 0:
             raise ValueError("ParentNode should have at least one child node")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
