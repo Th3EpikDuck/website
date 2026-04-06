e21e696b5adfd772b1ef21b0afa13534bbee9fb1
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-06 07:28:42 -0700

    Update test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 from htmlnode import HTMLNode
 from htmlnode import LeafNode
+from htmlnode import ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -53,6 +54,18 @@ class TestLeafNode(unittest.TestCase):
     def test_repr(self):
         node = LeafNode("p", "Hello", None)
         self.assertEqual(repr(node), "LeafNode(p, Hello, None)")
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+    child_node = LeafNode("span", "child")
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span><b>grandchild</b></span></div>")
 
 if __name__ == "__main__":
     unittest.main()
