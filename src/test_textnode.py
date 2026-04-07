5f18bd237edf6647f7721d0a17bd519e6475e87e
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-07 03:32:05 -0700

    Update test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -185,6 +185,18 @@ class TestTextNode(unittest.TestCase):
                 TextNode("code", TextType.CODE),
             ]
         )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_links(
+            "This is text with a link to ![to youtube](https://www.youtube.com"
+        )
+        self.assertListEqual([("to youtube", "https://www.youtube.com")], matches)
 
 if __name__ == "__main__":
     unittest.main()
