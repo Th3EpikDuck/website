b84706e9927c86e5a7493eec76871d394daf0922
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-10 19:02:10 -0700

    Update textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -71,53 +71,59 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     return re.findall(pattern, text)
 
-def split_nodes_image(old_nodes: List[Dict]) -> List[Dict]:
+def split_nodes_image(old_nodes):
     new_nodes = []
     image_pattern = re.compile(r'!\[([^\]]*)\]\(([^)]+)\)')
 
     for node in old_nodes:
-        if node.get("type") != "text":
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
-        text = node.get("text", "")
+
+        text = node.text
         last_index = 0
 
         for match in image_pattern.finditer(text):
             start, end = match.span()
             alt_text, url = match.groups()
+
             if start > last_index:
-                new_nodes.append({"type": "text", "text": text[last_index:start]})
-            new_nodes.append({"type": "image", "alt": alt_text, "url": url})
+                new_nodes.append(TextNode(text[last_index:start], TextType.TEXT))
+
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
             last_index = end
+
         if last_index < len(text):
-            new_nodes.append({"type": "text", "text": text[last_index:]})
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
 
     return new_nodes
 
 
-def split_nodes_link(old_nodes: List[Dict]) -> List[Dict]:
+def split_nodes_link(old_nodes):
     new_nodes = []
     link_pattern = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
 
     for node in old_nodes:
-        if node.get("type") != "text":
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
 
-        text = node.get("text", "")
+        text = node.text
         last_index = 0
 
         for match in link_pattern.finditer(text):
             start, end = match.span()
             link_text, url = match.groups()
-            if start > last_index:
-                new_nodes.append({"type": "text", "text": text[last_index:start]})
 
-            new_nodes.append({"type": "link", "text": link_text, "url": url})
+            if start > last_index:
+                new_nodes.append(TextNode(text[last_index:start], TextType.TEXT))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, url))
             last_index = end
-            
+
         if last_index < len(text):
-            new_nodes.append({"type": "text", "text": text[last_index:]})
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+
     return new_nodes
 
 def text_to_textnodes(text):
